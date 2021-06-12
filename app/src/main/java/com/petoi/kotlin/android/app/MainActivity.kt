af5bf21f107cc2e2e88a3315051271c536013f58
Orlando Chen <seagochen@hotmail.com>
2021-06-12 15:53:42 +0800

    popup menu done

--- app/src/main/java/com/petoi/kotlin/android/app/MainActivity.kt
+++ app/src/main/java/com/petoi/kotlin/android/app/MainActivity.kt
@@ -2,8 +2,10 @@ package com.petoi.kotlin.android.app
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import com.petoi.kotlin.android.app.bluetooth.BluetoothBasedActivity
+import com.petoi.kotlin.android.app.widgets.MainDropMenu
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class MainActivity : BluetoothBasedActivity() {
@@ -101,6 +103,12 @@ class MainActivity : BluetoothBasedActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
+//        Button btn = (Button) findViewById(R.id.btnShow)
+        val btn = findViewById<Button>(R.id.btn_main_menu)
+        val menu = MainDropMenu()
+        btn.setOnClickListener {
+            menu.showMenu(this, it)
+        }
 
         // 绑定Android控件和它们应该对应的消息事件
 //        bindSearchBtnView()
---
+++ app/src/main/java/com/petoi/kotlin/android/app/widgets/MainDropMenu.kt
@@ -0,0 +1,40 @@
+package com.petoi.kotlin.android.app.widgets
+
+import android.content.Context
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import com.petoi.kotlin.android.app.R
+
+
+open class MainDropMenu : PopupMenu.OnMenuItemClickListener {
+
+    fun showMenu(context: Context, v: View) {
+        val popup = PopupMenu(context, v)
+        popup.setOnMenuItemClickListener(this)
+        popup.inflate(R.menu.menu_main)
+        popup.show()
+    }
+
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            R.id.menu_main_calib -> {            // do your code
+                Log.d("MainDropMenu", "menu_main_calib")
+                // TODO
+                true
+            }
+
+            R.id.menu_main_help -> {
+                Log.d("MainDropMenu", "menu_main_help")
+                // TODO
+                true
+            }
+
+            else -> false
+        }
+    }
+}
+
+
