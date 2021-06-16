042096cf74b78585931f7b7f10344b784458dd10
Orlando Chen <seagochen@hotmail.com>
2021-06-16 15:10:20 +0800

    view jump

--- app/src/main/java/com/petoi/kotlin/android/app/CalibrationActivity.kt
+++ app/src/main/java/com/petoi/kotlin/android/app/CalibrationActivity.kt
@@ -1,12 +1,26 @@
 package com.petoi.kotlin.android.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.petoi.kotlin.android.app.bluetooth.BluetoothBasedActivity
 
-class CalibrationActivity : AppCompatActivity() {
+class CalibrationActivity : BluetoothBasedActivity() {
+
+    // 退回按钮
+    private fun bindBackBtn() {
+        val btn = findViewById<Button>(R.id.btn_calib_back)
+        btn.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calibration)
-        //TODO
+
+        bindBackBtn()
     }
 }
--- app/src/main/java/com/petoi/kotlin/android/app/widgets/MainPopupMenu.kt
+++ app/src/main/java/com/petoi/kotlin/android/app/widgets/MainPopupMenu.kt
@@ -1,16 +1,23 @@
 package com.petoi.kotlin.android.app.widgets
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
+import com.petoi.kotlin.android.app.CalibrationActivity
+import com.petoi.kotlin.android.app.DeviceConnectActivity
 import com.petoi.kotlin.android.app.R
 
 
 open class MainPopupMenu : PopupMenu.OnMenuItemClickListener {
 
+    var context: Context? = null
+
     fun showMenu(context: Context, v: View) {
+        this.context = context
+
         val popup = PopupMenu(context, v)
         popup.setOnMenuItemClickListener(this)
         popup.inflate(R.menu.menu_main)
@@ -22,13 +29,19 @@ open class MainPopupMenu : PopupMenu.OnMenuItemClickListener {
         return when (item!!.itemId) {
             R.id.menu_main_calib -> {            // do your code
                 Log.d("MainDropMenu", "menu_main_calib")
-                // TODO
+
+                // 跳转到校准界面
+                val intent = Intent(context, CalibrationActivity::class.java)
+                context?.startActivity(intent)
                 true
             }
 
-            R.id.menu_main_help -> {
-                Log.d("MainDropMenu", "menu_main_help")
-                // TODO
+            R.id.menu_main_connector -> {
+                Log.d("MainDropMenu", "menu_main_connector")
+
+                // 跳转到校准界面
+                val intent = Intent(context, DeviceConnectActivity::class.java)
+                context?.startActivity(intent)
                 true
             }
 
