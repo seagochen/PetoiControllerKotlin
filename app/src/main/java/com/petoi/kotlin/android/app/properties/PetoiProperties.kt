71605afd77a782a1ab16ba3b962036a9ec194923
Orlando Chen <seagochen@hotmail.com>
2021-06-29 13:29:48 +0800

    prepare play video

--- app/src/main/java/com/petoi/kotlin/android/app/properties/PetoiProperties.kt
+++ app/src/main/java/com/petoi/kotlin/android/app/properties/PetoiProperties.kt
@@ -1,8 +1,13 @@
 package com.petoi.kotlin.android.app.properties
 
 import android.content.Context
-import java.lang.Exception
+import java.text.SimpleDateFormat
 import java.util.*
+
+enum class PetoiDevice {
+    Bittle,
+    Nybble
+}
 
 class PetoiProperties {
 
@@ -18,5 +23,30 @@ class PetoiProperties {
                 return activityProperties
             }
         }
+    }
+
+    fun isFirstTime(context: Context): Boolean {
+        val properties = getProperties(context)
+        if (properties.containsKey("first_time")) {
+            return false
+        }
+        return true
+    }
+
+    fun setFirstTime(context: Context) {
+        val properties = getProperties(context)
+        val currentTime = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.getDefault())
+        properties.setProperty("first_time", currentTime.format(Date()))
+    }
+
+    fun getSelectedPetoiDevice(context: Context): PetoiDevice {
+        val properties = getProperties(context)
+        val currentDevice =  properties.get("petoi_device") as PetoiDevice
+        return currentDevice
+    }
+
+    fun setSelectedPetoiDevice(context: Context, device: PetoiDevice) {
+        val properties = getProperties(context)
+        properties.set("petoi_device", device)
     }
 }
---
+++ app/src/main/java/com/petoi/kotlin/android/app/widgets/VideoSurfaceHolder.kt
@@ -0,0 +1,61 @@
+package com.petoi.kotlin.android.app.widgets
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.view.Surface
+import android.view.SurfaceHolder
+
+class VideoSurfaceHolder: SurfaceHolder {
+    
+    override fun addCallback(callback: SurfaceHolder.Callback?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeCallback(callback: SurfaceHolder.Callback?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isCreating(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun setType(type: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setFixedSize(width: Int, height: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setSizeFromLayout() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setFormat(format: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setKeepScreenOn(screenOn: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun lockCanvas(): Canvas {
+        TODO("Not yet implemented")
+    }
+
+    override fun lockCanvas(dirty: Rect?): Canvas {
+        TODO("Not yet implemented")
+    }
+
+    override fun unlockCanvasAndPost(canvas: Canvas?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSurfaceFrame(): Rect {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSurface(): Surface {
+        TODO("Not yet implemented")
+    }
+}
